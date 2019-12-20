56d36ce4665f48923b9513416412b5be199d77cd
wusiyu <stephenwu2019@163.com>
2019-12-20 19:59:40 +0800

    chat style

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="chat">
     <div class="chat-top">chat</div>
-    <div class="chat-main">
-      main
+    <div class="chat-main">main</div>
+    <div class="chat-input">
+      <el-input class="chat-input__area" v-model="msg" type="textarea"></el-input>
     </div>
-    <div class="chat-input"></div>
   </div>
 </template>
 
@@ -12,7 +12,9 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 @Component({})
-export default class Chart extends Vue {}
+export default class Chart extends Vue {
+  private msg: string = "";
+}
 </script>
 
 <style lang="postcss" scoped>
@@ -26,13 +28,22 @@ export default class Chart extends Vue {}
     font-weight: 600;
     align-items: center;
     justify-content: center;
+    border-bottom: 1px solid var(--color-light-border);
   }
-  &-main{
+  &-main {
     flex: 1;
   }
-  &-input{
+  &-input {
     height: 100px;
-    margin: 5px;
+    border-top: 1px solid var(--color-light-border);
+    &__area {
+      height: 100%;
+      & >>> .el-textarea__inner {
+        border: none;
+        height: 100%;
+        resize: none;
+      }
+    }
   }
 }
 </style>
