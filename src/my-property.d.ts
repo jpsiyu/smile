5f8b956e02f60800d3457064470dc3d91d925335
wusiyu <stephenwu2019@163.com>
2019-12-21 15:46:52 +0800

    properties

--- src/my-property.d.ts
+++ src/my-property.d.ts
@@ -3,7 +3,7 @@ import Vue from 'vue'
 declare module 'vue/types/vue' {
   interface Vue {
     $axios: any;
-    $shh: any;
+    $visitor: any;
     $msgVerify: Function;
     $msgError: Function;
     $deleteEmpty: Function;
