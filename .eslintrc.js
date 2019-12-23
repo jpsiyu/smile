053141559f1a1e420751175cdc21e8e646632ca6
wusiyu <stephenwu2019@163.com>
2019-12-23 14:35:02 +0800

    off

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,7 @@ module.exports = {
     '@vue/typescript'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off'
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
